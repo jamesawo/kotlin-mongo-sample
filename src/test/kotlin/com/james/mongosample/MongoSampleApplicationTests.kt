1ab482ad7bb311b7f7057b22052f397450aff6fd
James Aworo <james.aworo@outlook.com>
2023-11-06 01:40:14 +0100

    add mongodb connection string in application.properties

---
+++ src/test/kotlin/com/james/mongosample/MongoSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.james.mongosample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MongoSampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
